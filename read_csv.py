47a339c949cf9e7c00353757b45debb810dc10ae
Santeri Moberg <santeri.moberg30@gmail.com>
2019-05-08 15:56:10 +0300

    se osaa filtteroida

--- read_csv.py
+++ read_csv.py
@@ -2,7 +2,13 @@ import numpy as np
 import pandas as pd
 
 def main():
-    df = pd.read_csv('tieliikenneonnettomuudet_2017_onnettomuus.csv', sep=';', encoding='latin-1')
+	df = pd.read_csv('tieliikenneonnettomuudet_2017_onnettomuus.csv', sep=';', encoding='latin-1', low_memory=False)
+	get_off_road(df)
+
+
+def get_off_road(df):
+	""" filters all the accidents where vehicle veered off the road """
+	df_offroad = df.loc[(df['Ontyyppi'] <= 89) & (df['Ontyyppi'] >= 80)]
 
 
 if __name__ == '__main__':
