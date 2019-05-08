4558eb3de2c624bf4f9dc822b05334747f2bcddc
Antti Männikkö <anttimmmmm@gmail.com>
2019-05-08 15:28:46 +0300

    Init

---
+++ read_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class Csv_reader:
+
+	def read_csv(self, csv_filepath):
+		#TODO: lukeminen
+		#pd.read_csv(csv_filepath, encoding = "utf-8")
+		
+
+if __name__ == '__main__':
+	csv = Csv_reader()
+	#csv.read_csv("./Data/2017/tieliikenneonnettomuudet_2017_onnettomuus.csv")
+	
